b95570f5fafb91fd4468cce6c2c9920b1897b178
Axou89 <senecal.axel89@gmail.com>
2023-06-08 12:15:38 +0200

    fix

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerData
+{
+    public string username;
+    public string password;
+
+    public string Stringify()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public static PlayerData Parse(string json)
+    {
+        return JsonUtility.FromJson<PlayerData>(json);
+    }
+}
